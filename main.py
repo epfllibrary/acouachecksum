7362f7be7f62e009c6d4e03407ae2eb42bab34c6
Alain Borel <alain.borel@epfl.ch>
2022-11-02 09:12:43 +0100

    basic error logging in place

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from unicodedata import normalize
 
 version = "0.6"
 
+error_file = "ACOUA_md5_errors.txt"
+
 def md5Checksum(filePath):
     with open(filePath, 'rb') as fh:
         m = hashlib.md5()
@@ -22,10 +24,13 @@ def md5Checksum(filePath):
 
 
 def runchecksum(tkroot):
-
+    # Clear all existing text messages
     for label in tkroot.winfo_children():
         if type(label) is Label:
             label.destroy()
+
+    error_message = f"There were errors or warnings during processing:\ncheck {error_file} for information."
+    error_file_header = "This is the acouachecksum log for errors and warnings. Do not archive.\n"
 
     d_title = "Select your ingestion folder"
     choosedir = filedialog.askdirectory(initialdir=Path.home(), title=d_title)
@@ -56,11 +61,26 @@ def runchecksum(tkroot):
                 files.append(filename)
 
     f = open("ACOUA_md5.md5", "wb")
+    f_err = open(error_file, "w")
+    f_err.write(error_file_header)
     for element in files:
-        md5 = md5Checksum(element)
-        # filenames must be encoded as UTF-8, or they might not match what Libsafe sees on the filesystem
-        # also: NFC normalization for proper (composed) representation of accented characters
-        f.write(normalize('NFC',f'{md5} {element.replace(choosedir,".")}\n').encode("UTF-8"))
+        try:
+            md5 = md5Checksum(element)
+            # filenames must be encoded as UTF-8, or they might not match what Libsafe sees on the filesystem
+            # also: NFC normalization for proper (composed) representation of accented characters
+            f.write(normalize('NFC',f'{md5} {element.replace(choosedir,".")}\n').encode("UTF-8"))
+        except Exception as e:
+            trace = str(e)
+            f_err.write(trace)
+
+    f_err.close()
+    f_err = open(error_file, "r")
+    error_content = f_err.read()
+    if error_content == error_file_header:
+        os.remove(error_file)
+    else:
+        error_info = Label(tkroot, text=error_message)
+        error_info.pack()
 
     f.close()
     done_info = Label(tkroot, text=f'Done.')
